1abf5fa85c09dc736fb990739aba3bd7f5c07e26
ArcticFoxJ <j.j.isaacs@hotmail.co.uk>
2023-12-06 15:57:11 +0000

    simplify day 2

--- 2023/Day2/Day2Solution.cs
+++ 2023/Day2/Day2Solution.cs
@@ -16,41 +16,21 @@ namespace AdventOfCode._2023.Day2
             };
 
             var possibleIdTotal = 0;
-            var gameCounts = new Dictionary<int, IDictionary<string, int>>();
-            foreach (var game in games)
-            {
-                var gameData = game.Split(':');
-                var gameId = int.Parse(gameData[0].Replace("Game ", ""));
-                var maxCounts = colors.ToDictionary(x => x, x => 0);
-                gameCounts.Add(gameId, maxCounts);
-
-                var gameCubes = gameData[1].Split(';');
-                foreach (var cubeGroup in gameCubes)
-                {
-                    var matches = Regex.Matches(cubeGroup, string.Format(@"\D*(?<count>\d+) (?<color>{0})", $"{string.Join('|', colors)}"));
-                    foreach(Match match in matches)
+            Dictionary<int, Dictionary<string, int>> gameCounts = games.ToDictionary(
+                x => int.Parse(Regex.Match(x, @"Game (?<id>\d*):").Groups["id"].Value),
+                x => colors.ToDictionary(
+                    c => c, 
+                    c =>
                     {
-                        var color = match.Groups["color"].Value;
-                        var count = int.Parse(match.Groups["count"].Value);
-                        gameCounts[gameId][color] = gameCounts[gameId][color] > count ? gameCounts[gameId][color] : count;
-                    }
-                }
-            }
+                        var colorGroups = Regex.Matches(x, string.Format(@"\D*(?<count>\d+) (?<color>{0})", $"{string.Join('|', colors)}"));
+                        return colorGroups.Where(y => y.Groups["color"].Value == c).Select(y => int.Parse(y.Groups["count"].Value)).Max();
+                    })
+            );
 
-            foreach (var gameCount in gameCounts)
+            var validGames = gameCounts.Where(x => x.Value.All(y => y.Value <= cubeTotals[y.Key]));
+            foreach (var validGame in validGames)
             {
-                var validGame = true;
-                foreach(var colorCount in gameCount.Value)
-                {
-                    if(colorCount.Value > cubeTotals[colorCount.Key])
-                    {
-                        validGame = false;
-                    }
-                }
-                if(validGame)
-                {
-                    possibleIdTotal += gameCount.Key;
-                }
+                possibleIdTotal += validGame.Key;
             }
 
             return possibleIdTotal; //2617
